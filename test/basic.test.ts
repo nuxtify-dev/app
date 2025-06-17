8c595cf9c92a233f5f7075d8a512e20d82979951
David Stack <2013388+davidstackio@users.noreply.github.com>
2025-06-17 00:13:20 -0400

    chore: add accurate test

--- test/basic.test.ts
+++ test/basic.test.ts
@@ -2,14 +2,14 @@ import { fileURLToPath } from 'node:url'
 import { describe, it, expect } from 'vitest'
 import { setup, $fetch } from '@nuxt/test-utils/e2e'
 
-describe('ssr', async () => {
+describe('nuxt', async () => {
   await setup({
     rootDir: fileURLToPath(new URL('./fixtures/basic', import.meta.url)),
   })
 
-  it('renders the index page', async () => {
+  it('renders the nuxt entrypoint', async () => {
     // Get response to a server-rendered page with `$fetch`.
     const html = await $fetch('/')
-    expect(html).toContain('<div>basic</div>')
+    expect(html).toContain('<div id="__nuxt">')
   })
 })
