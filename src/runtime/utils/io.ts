2a1dd7e1f109da7d186cdd2c371edff4cdc91103
David Stack <2013388+davidstackio@users.noreply.github.com>
2025-06-20 14:13:16 -0400

    refactor: replace papaparse with csv-stringify

--- src/runtime/utils/io.ts
+++ src/runtime/utils/io.ts
@@ -1,9 +1,14 @@
 import saveAs from 'file-saver'
-import Papa from 'papaparse'
+import { stringify } from 'csv-stringify/browser/esm/sync'
 
+/**
+ * Converts an array of data into a CSV string and initiates a download.
+ */
 export function downloadCSV(data: unknown[], filename = 'export') {
   // Convert to CSV
-  const csv = Papa.unparse(data)
+  const csv = stringify(data, {
+    header: true,
+  })
 
   // Download File
   const blob = new Blob([csv], {
