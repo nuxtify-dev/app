679dba2083e4b1d1b88e8c13cc41d1de58b7d628
David Stack <2013388+davidstackio@users.noreply.github.com>
2025-08-04 10:42:38 -0400

    fix: merge, don't replace vuetify options

--- src/runtime/plugins/vuetify.ts
+++ src/runtime/plugins/vuetify.ts
@@ -1,10 +1,11 @@
+import { defu } from 'defu'
 import { defineNuxtPlugin } from '#imports'
 
 export default defineNuxtPlugin((nuxtApp) => {
   // Override default Vuetify options
   nuxtApp.hook('vuetify:before-create', ({ vuetifyOptions }) => {
-    vuetifyOptions.defaults = {
+    vuetifyOptions.defaults = defu(vuetifyOptions.defaults, {
       VNavigationDrawer: { floating: true },
-    }
+    })
   })
 })
