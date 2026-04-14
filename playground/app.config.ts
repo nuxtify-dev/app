62a9a49a7e6997de7930b8ca03af381326e393b0
David Stack <2013388+davidstackio@users.noreply.github.com>
2026-04-14 15:37:53 -0400

    refactor(playground): use app.config

---
+++ playground/app.config.ts
@@ -0,0 +1,162 @@
+import {
+  mdiAccountCog,
+  mdiAccountMultiple,
+  mdiAccountPlus,
+  mdiCommentQuote,
+  mdiFirebase,
+  mdiForum,
+  mdiHome,
+  mdiLifebuoy,
+  mdiLogin,
+  mdiTelevisionPlay,
+  mdiTestTube,
+  mdiViewDashboard,
+} from '@mdi/js'
+
+export default defineAppConfig({
+  nuxtify: {
+    // Brand
+    brand: {
+      logo: {
+        lightUrl: '/img/nuxtify-logo-light.png',
+        darkUrl: '/img/nuxtify-logo-dark.png',
+      },
+      tagline: 'This is a sample tagline for the app module.',
+    },
+
+    // Email
+    email: {
+      general: 'hello@nuxtify.dev',
+      support: 'support@nuxtify.dev',
+    },
+
+    // Auth
+    auth: {
+      adminDomain: 'nuxtify.dev',
+    },
+
+    // Navigation
+    navigation: {
+      // Main nav
+      primary: [
+        {
+          text: 'Home',
+          to: '/',
+          icon: mdiHome,
+        },
+        {
+          text: 'Firebase',
+          to: '/firebase',
+          icon: mdiFirebase,
+        },
+        {
+          text: 'Test',
+          to: '/test',
+          icon: mdiTestTube,
+        },
+        {
+          text: 'Sign In',
+          to: '/signin',
+          icon: mdiLogin,
+        },
+        {
+          text: 'Sign Up',
+          to: '/signup',
+          icon: mdiAccountPlus,
+        },
+      ],
+      // User menu
+      secondary: [
+        {
+          text: 'Account',
+          to: '/account',
+          icon: mdiAccountCog,
+        },
+      ],
+      // Help menu
+      altPrimary: [
+        {
+          title: '',
+          links: [
+            {
+              text: 'Getting Started',
+              href: '/signup/complete',
+              icon: mdiTelevisionPlay,
+            },
+          ],
+        },
+        {
+          title: 'GET IN TOUCH',
+          links: [
+            {
+              text: 'Join Our Community',
+              href: '/community',
+              icon: mdiForum,
+              openInNew: true,
+            },
+            {
+              text: 'Help & Support',
+              to: '/support',
+              icon: mdiLifebuoy,
+            },
+            {
+              text: 'Leave A Review',
+              to: '/review',
+              icon: mdiCommentQuote,
+            },
+          ],
+        },
+      ],
+      // Overflow menu
+      altSecondary: [
+        {
+          text: 'About',
+          href: 'https://nuxtify.dev/about',
+        },
+        {
+          text: 'Contact',
+          href: 'https://nuxtify.dev/contact',
+        },
+        {
+          text: 'Articles',
+          href: 'https://nuxtify.dev/articles',
+        },
+        {
+          text: 'Accessibility',
+          href: 'https://nuxtify.dev/accessibility',
+        },
+        {
+          text: 'Privacy',
+          href: 'https://nuxtify.dev/privacy',
+        },
+        {
+          text: 'Terms',
+          href: 'https://nuxtify.dev/terms',
+        },
+      ],
+      // Admin
+      admin: [
+        {
+          text: 'Dashboard',
+          to: '/admin',
+          icon: mdiViewDashboard,
+        },
+        {
+          text: 'Users',
+          to: '/admin/users',
+          icon: mdiAccountMultiple,
+        },
+      ],
+    },
+
+    // Credits
+    credits: {
+      creator: {
+        name: 'Nuxtify',
+        domain: 'nuxtify.dev',
+      },
+      prependText: 'Made with love by',
+      appendText: ' Ship weirdly fast.',
+    },
+  },
+})
