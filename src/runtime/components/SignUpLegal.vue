bd07a38a8dcc83f1c762a09009ac26d74bb06634
David Stack <2013388+davidstackio@users.noreply.github.com>
2026-06-25 17:02:20 -0400

    fix: don't hard code policies

--- src/runtime/components/SignUpLegal.vue
+++ src/runtime/components/SignUpLegal.vue
@@ -9,13 +9,13 @@ const nuxtifyConfig = useNuxtifyConfig()
   <div class="text-center text-caption text-medium-emphasis mb-6">
     By continuing, you acknowledge that you've read and agree to our
     <a
-      :href="`https://${nuxtifyConfig.brand?.domain}/terms`"
+      :href="`https://${nuxtifyConfig.brand?.domain}${nuxtifyConfig.routes.terms}`"
       target="_blank"
       rel="noopener noreferrer"
       class="text-primary text-decoration-none"
     >
       Terms</a> and <a
-      :href="`https://${nuxtifyConfig.brand?.domain}/privacy`"
+      :href="`https://${nuxtifyConfig.brand?.domain}${nuxtifyConfig.routes.privacy}`"
       target="_blank"
       rel="noopener noreferrer"
       class="text-primary text-decoration-none"
